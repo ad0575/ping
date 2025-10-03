901b51e532097a238668fa6e14284ea7978356dc
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2025-10-03 11:41:03 -0300

    Add app name to user

--- app/src/main/java/ag/ping/MainActivity.java
+++ app/src/main/java/ag/ping/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.SpannableString;
 import android.text.method.ScrollingMovementMethod;
+import android.text.style.RelativeSizeSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +29,10 @@ public class MainActivity extends Activity {
         this.setContentView(R.layout.main);
 
         Button btn = findViewById(R.id.host_btn);
+        String app = getString(R.string.app_name),
+            help = getString(R.string.app_help);
+        SpannableString textSpan = new SpannableString(
+                app+"\n"+help);
 
         i18n = new MainI18N(getResources());
         output = findViewById(R.id.output);
@@ -44,6 +50,12 @@ public class MainActivity extends Activity {
                 doPing(target);
             }
         });
+        textSpan.setSpan(
+                new RelativeSizeSpan(2.0f), // Up text 2x
+                0, app.length(),
+                SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+        output.append(textSpan);
         output.setMovementMethod(new ScrollingMovementMethod());
     }
 
